49f19b2c89541ffa35f846eed432538b386f4043
Broken Rice Films <brokenricefilms@gmail.com>
2024-07-03 12:35:59 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,4 +1,69 @@
 export const feeds = [
- // vlog & travel
-'https://www.upwork.com/ab/feed/jobs/rss?ontology_skill_uid=1031626793223176192&paging=NaN-undefined&q=(vlog%20OR%20travel)&sort=recency&api_params=1&securityToken=34408095ab25678a2024d37cb7a31922a7f3c60b617b38301086ad907b49281b3fe1862ed0a272e4418fd7a17c3bb892cf0fb61fc924f8c981d126bb5b42fad4&userUid=1772236613276483584&orgUid=1772236613276483585'
+ // Chris Young
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCvVsD2hFZRgKNH7x5Q1wwug',
+  // Cullen Kelly
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCHZD22Eei2KX8wpJUzk0Bxg',
+  // Danny Gevirtz
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC34ZUMA3kx-51dREoXLGDZQ',
+  // Darren Mostyn
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC6LqojU1x4LWWhL7QqLB8Mw',
+  // Digital Spaghetti
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTdsYrCpF2zRKWWL0SkaOtA',
+  // Doobydobap
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCh8gHdtzO2tXd593_bjErWg',
+  // HillierSmith
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCjRCWxEBiqV8yqwxX7SsYkA',
+  // Jake Felzien
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCIFdtLXB_6YDoiJoe1k5wOA',
+  // James Smith
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCOFxOq3mmdMly4A9JSzcf6Q',
+  // Jimmy Kim
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCOU2PEQuXiz4JsfEtW3frhA',
+  // Johnny Harris
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCmGSJVG3mCRXVOP4yZrU1Dw',
+  // Khoai Lang Thang
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCZE88kYvCKUKjM-G0uc8Duw',
+  // Kyrie Likhodkin
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCwL_k6kpBiCWBRBk4ZjSuLA',
+  // Lrnjulie
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC8Oi0GUllFCc0mZrKVmErvg',
+  // Make art not content.
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UClNikN9BUpb0K7xNB33VihQ',
+  // Mark Bone
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4exYtWOlHd4mL-owyyA12w',
+  // Natalie Lynn
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
+  // CRAZY BROKE ASIAN
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCQDjC9axwfSnYUuO5_d0xYg',
+  // Phillip Q Vu
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCtDxlEsiIM4PSawQX1DKPlw',
+  // Ryan Ng Films
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC5oaZRTgavIEw6oz_fXu7Pg',
+  // Samuel Nam
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCovagaiL6pBYWfvThaXVniA',
+  // Taeha Types
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCMHXMAeKkI6HXlPfLiYvo9g',
+  // The Editing Podcast
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCJJV8pckL_OiW-IpzwPqAfw',
+  // This Guy Edits
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCcPuBEAwuF6XWXkcXJXJwsg',
+  // wholesomesimon
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCBk1w-2BLkm5L72tc0eI74g',
+  // Yat Fung Leung
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCRqtAQ61-inw_8oMpEb4mug',
+  // Cooking Hobbyist Guy, Yonam
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCyozK5OFN5lDrwim5wqQnLA',
+  // Blaine Westropp
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkL_KReMi40wcPeTMXGSeAw',
+  // Paul E.T.
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC6me-RzbQFQ-kRyr6BlGZWg',
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCUQo7nzH1sXVpzL92VesANw',
+  // dannyrio
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCpI1aSij9qlOe01Kh2Y2aWw',
+  // Michael Reeves
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCtHaxi4GTYDpJgMSGy7AeSw',
+  // Peej ENT
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCHpIQdMhq64uBfk25llvHmA',
+  //Joshua Kirk
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC8mv1jxVbta9thZUP1uHeug',
 ]
