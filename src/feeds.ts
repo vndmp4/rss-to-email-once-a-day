985806a07462e0800953af06a657e401f2f27fbe
Broken Rice Films <brokenricefilms@gmail.com>
2024-11-13 21:23:14 +0700

    👌Auto commit

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,36 +1,3 @@
 export const feeds = [
-  ///// Video
-  // Chris Young
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCvVsD2hFZRgKNH7x5Q1wwug',
-  // Cullen Kelly
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCHZD22Eei2KX8wpJUzk0Bxg',
-  // Digital Spaghetti
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCTdsYrCpF2zRKWWL0SkaOtA',
-  // Doobydobap
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCh8gHdtzO2tXd593_bjErWg',
-  // HillierSmith
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCjRCWxEBiqV8yqwxX7SsYkA',
-  // Jimmy Kim
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCOU2PEQuXiz4JsfEtW3frhA',
-  // Make art not content.
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UClNikN9BUpb0K7xNB33VihQ',
-  // Natalie Lynn
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCkjrBN_GAjFJyVvjcI07KkQ',
-  // CRAZY BROKE ASIAN
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCQDjC9axwfSnYUuO5_d0xYg',
-  // The Editing Podcast
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCJJV8pckL_OiW-IpzwPqAfw',
-  // This Guy Edits
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCcPuBEAwuF6XWXkcXJXJwsg',
-  // wholesomesimon
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCBk1w-2BLkm5L72tc0eI74g',
-  // Yat Fung Leung
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCRqtAQ61-inw_8oMpEb4mug',
-  // dannyrio
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCpI1aSij9qlOe01Kh2Y2aWw',
-  // Sean
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCLm9mtUJlmbEjMFHD-0jcrw',
-
-  ///// blog
   'https://geohot.github.io/blog/feed.xml',
 ]
